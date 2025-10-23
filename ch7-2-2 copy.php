ac505555dc06e96aadd7dd06eb24c256f802a0e3
ray000518 <C112181129@nkust.edu.tw>
2025-10-23 12:09:53 +0800

    sec

---
+++ ch7-2-2 copy.php
@@ -0,0 +1,11 @@
+
+<?php 
+
+$arr = array( "SID"=>"C112181129",
+              "name"=> "黃亭睿", 
+              "shape"=>"三角形",
+               100 ); 
+
+foreach($arr as $key=>$value) echo "$key=>$value <br>";
+
+
